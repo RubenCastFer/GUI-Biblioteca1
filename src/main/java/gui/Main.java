025953488ab89dade30d0ca9916be22e6860bd08
RubenCF <ruben.castellano.fernandez.al@iespoligonosur.org>
2021-04-09 11:22:42 +0200

    principio

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -3,7 +3,7 @@
  */
 package gui;
 
-import javafx.application.Application;
+/*import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -22,5 +22,41 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+}*/
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+ 
+public class Main extends Application {
+ 
+    @Override
+    public void start(Stage primaryStage) {
+ 
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("viewsandcontrollers/main/Main.fxml"));
+            // Cargo la ventana
+            Pane ventana = (Pane) loader.load();
+ 
+            // Cargo el scene
+            Scene scene = new Scene(ventana);
+ 
+            // Seteo la scene y la muestro
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+ 
 }
+
 
